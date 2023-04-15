8c92dbead9bc5a4b48465e960cdfc65d56a8b231
saurabh singh <86177535+saurabhsngh5484@users.noreply.github.com>
2023-04-15 10:23:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function displayTimer(){
+	var date=new Date();
+	var time=date.toLocaleTimeString();
+	var options={day:'numeric' ,month:'long', year:'numeric'};
+	var today=date.toLocaleDateString(undefined , options);	
+	document.getElementById("timer").innerHTML=today + time;
+}
+setInterval(displayTimer, 1000);
